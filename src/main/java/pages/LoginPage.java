bc7de105de0cede8ea6a3b9494a5e65c25f9b7ec
apple <apple@MacBook-Pro.local>
2025-06-16 16:43:43 +0530

    Steps for login page has been created

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,4 +1,50 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+
+    WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+
+    @FindBy(xpath="//input[@id='email' and @name='login[username]']")
+    private WebElement emailInput;
+
+    @FindBy(xpath = "//input[@id='pass' and @name='login[password]']")
+    private WebElement passwordInput;
+
+    @FindBy(xpath = "//button[contains(@class,'action login primary')]")
+    private WebElement signInButton;
+
+    @FindBy(xpath = "//span[contains(text(),'Welcome')]")
+    private WebElement welcomeTitle; // Can be used to verify login
+
+    // Actions
+    public void enterEmail(String email) {
+        emailInput.sendKeys(email);
+    }
+
+    public void enterPassword(String password) {
+        passwordInput.sendKeys(password);
+    }
+
+    public void clickSignIn() {
+        signInButton.click();
+    }
+
+    public boolean isLoginSuccessful() {
+        try {
+            return welcomeTitle.isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
