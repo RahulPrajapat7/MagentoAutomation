93f3a3d86e2bb760906a6f0ab025709222a546c2
apple <apple@MacBook-Pro.local>
2025-06-16 16:14:26 +0530

    Step definitions implemented for signup

---
+++ src/test/java/stepDefinitions/SignupLoginSteps.java
@@ -0,0 +1,35 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.*;
+import org.openqa.selenium.WebDriver;
+import pages.SignupPage;
+import utils.BaseClass;
+
+public class SignupLoginSteps {
+
+    WebDriver driver = BaseClass.getDriver();
+    SignupPage signupPage = new SignupPage(driver);
+
+    @Given("User launches the Magento signup page")
+    public void user_launches_the_signup_page() {
+        driver.get("https://magento.softwaretestingboard.com/customer/account/create/");
+    }
+
+    @When("User enters first name, last name, email and password")
+    public void user_enters_signup_details() {
+        signupPage.enterDetails("Rahul", "Prajapati", "rahul123@gmail.com", "Test@1234");
+    }
+
+    @When("User clicks on create an account")
+    public void user_clicks_on_create_account() {
+        signupPage.submitForm();
+    }
+
+    @Then("User account should be created successfully")
+    public void user_account_should_be_created_successfully() {
+        String title = driver.getTitle();
+        if (!title.contains("My Account")) {
+            throw new AssertionError("Account creation failed!");
+        }
+    }
+}
---
+++ src/test/java/utils/BaseClass.java
@@ -0,0 +1,25 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            WebDriverManager.chromedriver().setup(); // Auto-downloads matching driver
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+        }
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
